93f2a4568f4ab7a90f91a7c83e00a35e2bf82025
EunKo <koeun0529@gmail.com>
2020-06-01 14:07:45 +0900

    template matching success

--- featurematching.py
+++ featurematching.py
@@ -1,23 +1,16 @@
 import numpy as np
 import cv2
 
-def featureMatching():
-    img1=cv2.imread('images/clothes.jpg',cv2.IMREAD_GRAYSCALE)
-    img2=cv2.imread('images/clothes-4.jpg',cv2.IMREAD_GRAYSCALE)
-    res=None
+img = cv2.imread("ori.jpg")
+imgray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+w, h = imgray.shape[:2]
+templ = cv2.imread("templ.jpg", cv2.IMREAD_GRAYSCALE)
+templ_h, templ_w = templ.shape[:2]
+res = cv2.matchTemplate(imgray, templ, cv2.TM_CCOEFF_NORMED)
+loc = np.where(res >= 0.5)
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
+cv2.imshow("res",img)
+cv2.waitKey(0)
 
-    orb=cv2.ORB_create()
-    kp1,des1=orb.detectAndCompute(img1,None)
-    kp2,des2=orb.detectAndCompute(img2,None)
-
-    bf=cv2.BFMatcher(cv2.NORM_HAMMING,crossCheck=True)
-    matches=bf.match(des1,des2)
-
-    matches=sorted(matches,key=lambda x:x.distance)
-    res=cv2.drawMatches(img1,kp1,img2,kp2,matches[:30],res,flags=0)
-
-    cv2.imshow('Feature Matching',res)
-    cv2.waitKey(0)
-    cv2.destrpyAllWindows()
-
-featureMatching()
+print(res)
